e383b13cced4171c682645da595fa417e47113a4
oleg <olezhka1629@gmail.com>
2023-11-04 00:28:45 +0300

    add base fluent api configuration for entities

---
+++ Library/Library.Persistance/EntitiesConfiguration/AuthorConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Library.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Persistance.EntitiesConfiguration
+{
+    public class AuthorConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.HasKey(author => author.Id);
+            builder.Property(author => author.FirstName).IsRequired()
+                                                        .HasMaxLength(100);
+            
+        }
+    }
+}
---
+++ Library/Library.Persistance/EntitiesConfiguration/BookConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Library.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Persistance.EntitiesConfiguration
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasKey(book => book.Id);
+            builder.Property(book => book.Title).HasMaxLength(255)
+        }
+    }
+}
