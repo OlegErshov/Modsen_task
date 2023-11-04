56107c2dd5c480bc562136ab134c1fe88911a223
oleg <olezhka1629@gmail.com>
2023-11-04 13:03:15 +0300

    add Update command and it's handler

---
+++ Library/Library.Application.Commands/AuthorCommands/UpdateCommand/UpdateAuthorCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Application.Commands.AuthorCommands.UpdateCommand
+{
+    public class UpdateAuthorCommand : IRequest<Unit>
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public Guid Id { get; set; }
+    }
+}
