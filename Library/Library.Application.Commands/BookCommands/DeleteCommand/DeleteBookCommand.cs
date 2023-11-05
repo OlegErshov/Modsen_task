6744287b77798901f3643152ab961965151053ae
oleg <olezhka1629@gmail.com>
2023-11-05 18:07:09 +0300

    add get book by id querie

--- Library/Library.Application.Commands/BookCommands/DeleteCommand/DeleteBookCommand.cs
+++ Library/Library.Application.Commands/BookCommands/DeleteCommand/DeleteBookCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Library.Application.Commands.BookCommands.DeleteCommand
 {
-    internal class DeleteBookCommand
+    public class DeleteBookCommand : IRequest<Unit>
     {
+         public Guid Id { get; set; }
     }
 }
