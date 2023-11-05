1e45695f9f7a3579d47529046b689a5dff17ab16
oleg <olezhka1629@gmail.com>
2023-11-05 17:31:56 +0300

    add book consstructor and create book command

---
+++ Library/Library.Application.Commands/BookCommands/CreateCommand/CreateBookCommand.cs
@@ -0,0 +1,21 @@
+﻿using Library.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Application.Commands.BookCommands.CreateCommand
+{
+    public class CreateBookCommand : IRequest<Unit>
+    {
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public string Description { get; set; }
+        public DateTime RecieveDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public Author  Author { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
--- Library/Library.Domain/Entities/Book.cs
+++ Library/Library.Domain/Entities/Book.cs
@@ -8,6 +8,19 @@ namespace Library.Domain.Entities
 {
     public class Book : IEntity
     {
+
+        public Book(Guid id, string title,string isbn, string description, DateTime receiveDate, 
+                    DateTime returnDate, Author author, Genre genre) 
+        { 
+            Id = id;
+            Title = title;
+            ISBN = isbn;
+            Description = description;
+            RecieveDate = receiveDate;
+            ReturnDate = returnDate;
+            Author = author;
+            Genre = genre;
+        }
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string ISBN { get; set; }
