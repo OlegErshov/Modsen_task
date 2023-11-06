ba77e482a0b3c573234b3faca106d9f2f09e328d
oleg <olezhka1629@gmail.com>
2023-11-06 16:40:29 +0300

    add querie to get all books

--- Library/Library.Application.Queries/BookQueries/GetBookQueries/GetByIdQuerie/GetBookByIdQuerie.cs
+++ Library/Library.Application.Queries/BookQueries/GetBookQueries/GetByIdQuerie/GetBookByIdQuerie.cs
@@ -6,9 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Library.Application.Queries.BookQueries.GetByIdQuerie
+namespace Library.Application.Queries.BookQueries.GetBookQueries.GetByIdQuerie
 {
-    public class GetBookByIdQuerie : IRequest<Book>
+    public class GetBookByIdQuerie : IRequest<BookReply>
     {
         public Guid Id { get; set; }
     }
---
+++ Library/Library.Application.Queries/BookQueries/GetBooksListQueries/BooksListReply.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Library.Application.Queries.AuthorQueries.GetByIdQuerie;
+using Library.Domain.Entities;
+using Library.Domain.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Application.Queries.BookQueries.GetBooksListQueries
+{
+    public class BooksListReply 
+    {   
+        public IList<BooksListDTO> Books { get; set; }
+    }
+}
